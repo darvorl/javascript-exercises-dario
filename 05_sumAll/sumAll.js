68d2fb6424d9d50e29cf91ec7311cd486b6e46b8
Dario <valentinorodriguezlozada@gmail.com>
2024-08-07 10:09:12 -0500

    Solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,37 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    const numbers = [];
 
+    if (
+        typeof a !== "number" || 
+        typeof b !== "number" ||
+        !Number.isInteger(a) || 
+        !Number.isInteger(b) ||
+        a < 0 || 
+        b < 0
+      ) {
+        return "ERROR";
+      }
+      
+
+    if (a > b) {
+        for (let i = b; i <= a; i++) {
+            numbers.push(i)
+        }
+    } else {
+        for (let i = a; i <= b; i++) {
+            numbers.push(i)
+        }
+    }
+
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+
+    return sum
 };
+
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
