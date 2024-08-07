ee63b8f47e4a4bac1a579520135bdc29a451ad77
Dario <valentinorodriguezlozada@gmail.com>
2024-08-06 20:18:43 -0500

    Solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const stringArr = string.split("");
+    const reversedArr = stringArr.reverse();
+    const reversedString = reversedArr.join("");
+    return reversedString
 };
 
+console.log(reverseString("123456789"))
 // Do not edit below this line
 module.exports = reverseString;
